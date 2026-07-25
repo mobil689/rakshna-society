d2c910949914a7f7cf374bb7eb351a5670a4a0e1
Abhisht Pratap Shukla <abhisht2021@gmail.com>
2026-07-25 20:07:25 +0530

    further fixes in news webpage

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -430,9 +430,9 @@ const News = () => {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div key={selectedSource} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {displayedArticles.map((article, idx) => (
-                  <NewsCard key={article.id} article={article} index={idx} />
+                  <NewsCard key={`${article.source}-${article.id}`} article={article} index={idx} />
                 ))}
               </div>
 
